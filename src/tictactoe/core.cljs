51a8aed2f10602de8a05248fab1721bd9f6f7fad
Yann Vanhalewyn <yann.vanhalewyn@gmail.com>
2016-02-07 07:47:44 +0100

    Got circles and crosses

--- src/tictactoe/core.cljs
+++ src/tictactoe/core.cljs
@@ -14,15 +14,36 @@
       j (range (count (:board @app-state))) ]
   (prn i j))
 
-(defn circle []
+(defn circle [x y]
+  (prn "Ok!")
   [:circle
    {
-    :r 25
-    :cx 30
-    :cy 30
+    :r 0.4
+    :cx (+ x 0.5)
+    :cy (+ y 0.5)
     :fill "none"
-    :stroke-width "8"
+    :stroke-width "0.1"
     :stroke "#5b8"}])
+
+(defn blank [x y]
+  [:rect
+   {:width 0.9
+    :height 0.9
+    :fill "#546"
+    :key (str x y)
+    :x (+ 0.05 x)
+    :y (+ 0.05 y)
+    :on-click (fn [] (swap! app-state update-in [:board y x] inc))}])
+
+(defn cross [x y]
+  [:g {:stroke "#b45"
+       :stroke-width 0.35
+       :stroke-linecap "round"
+       :transform
+       (str "translate(" (+ 0.5 x) "," (+ 0.5 y) ") "
+            "scale(0.35)")}
+   [:line {:x1 -1 :y1 -1 :x2 1 :y2 1}]
+   [:line {:x1 1 :y1 -1 :x2 -1 :y2 1}]])
 
 (defn board []
   (into
@@ -32,19 +53,16 @@
      :height 500}]
    (for [i (range (count (:board @app-state)))
          j (range (count (:board @app-state))) ]
-     [:rect {:width 0.9
-             :height 0.9
-             ;; :fill "#546"
-             :fill (if (zero? (get-in @app-state [:board j i]))
-                     "green"
-                     "yellow")
-             :key (str i j)
-             :x i
-             :y j
-             :on-click (fn [] (swap! app-state update-in [:board j i] inc))}])))
+     (case (get-in @app-state [:board j i])
+       0 [blank i j]
+       1 [circle i j]
+       2 [cross i j])
+     )
+   ))
 
 (defn app []
   [:div
+   [cross 1 1]
    [:h1 (:text @app-state)]
    [:button {:on-click #(prn @app-state)} "Print State"]
    [board]])
@@ -56,4 +74,5 @@
 (defn on-js-reload []
   ;; optionally touch your app-state to force rerendering depending on
   ;; your application
+  (swap! app-state assoc-in [:board 0 0] 2)
 )
